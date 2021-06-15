f41875844d4cb44b38b7cdf2288f511d4487217f
Du Erica <erica.du0916@gmail.com>
2021-06-15 13:28:37 +0800

    new init

---
+++ app/assets/scripts/main.js
@@ -0,0 +1,15 @@
+// $(function(){
+// 	$('.tab-title a:first').addClass('active')
+// 	$('.tab-cont').hide()
+// 	$('.tab-cont:first').show()
+
+// 	$('.tab-title a').on('click', function(){
+// 		$('.tab-title a').removeClass('active')
+// 		$(this).addClass('active')
+// 		$('.tab-cont').hide()
+        
+// 		let target = $(this).attr('href')
+// 		$(target).fadeIn()
+// 		return false
+// 	}) 
+// })
