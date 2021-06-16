3ce1686a520e5caba9645f7ed94f8f31144ba5ca
Du Erica <erica.du0916@gmail.com>
2021-06-16 22:13:34 +0800

    Update 2021-06-16T14:13:30.422Z

--- assets/scripts/all.js
+++ assets/scripts/all.js
@@ -2,14 +2,16 @@
 
 $(function () {
 	$('.tabs-head a:first').addClass('active')
-	$('.tab-body').hide()
-	$('.tab-body:first').show()
+	$('.tabs-cont').hide()
+	$('.tabs-cont:first').show()
 	$('.tabs-head a').on('click', function () {
 		$('.tabs-head a').removeClass('active')
 		$(this).addClass('active')
-		$('.tab-body').hide()
+		$('.tabs-cont').hide()
 		let target = $(this).attr('href')
 		$(target).fadeIn()
 		return false
 	})
 })
+
+//# sourceMappingURL=all.js.map
